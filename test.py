ed42555eb69af0635422a52118e4ef69790a8680
brandon <brandon@ip-10-159-51-75.ec2.internal>
2015-12-01 20:02:41 +0000

    updated test code

--- test.py
+++ test.py
@@ -6,19 +6,23 @@ from sys import argv,exit
 from credentials import CREDENTIALS
 from json import dumps
 
+t = ['un','una','yo','tu','su','y','en','para','por','a','i','u']
+language = 'es'
+
 class StdOutListener(StreamListener):
     def on_status(self, status):
-	print dumps(status._json,indent=4,sort_keys=True)
+	tweet = dumps(status._json)
+	filter_used = ','.join(t)
+	print '~'.join([filter_used, language, tweet])
      
     def on_error(self, status):
         print status
 
 if __name__=='__main__':
     # t = argv[1] if len(argv)>1 else 'boom'
-    t = ['un','una','yo','tu','su','y','en','para','por','a','i','u']
     auth = OAuthHandler(CREDENTIALS['KEY'],CREDENTIALS['SECRET'])
     auth.set_access_token(CREDENTIALS['OAUTH_TOKEN'], \
 	CREDENTIALS['OAUTH_SECRET'])
     api = API(auth)
     stream = Stream(auth=api.auth, listener=StdOutListener())
-    stream.filter( track=t ,languages=['es'])
+    stream.filter( track=t ,languages=[language])
