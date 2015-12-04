76be228422b8c7d7fb0ee02d36ef0c77da22bf58
Brandon Shurick <bshurick@berkeley.edu>
2015-12-04 13:33:09 +0000

    fix new raw directory

--- repair_last_hour.sh
+++ repair_last_hour.sh
@@ -5,12 +5,12 @@ lastday=$(date -d'-1 day' +%Y%m%d)
 sudo rm /data/tmp/$lasthr\.txt.gz
 sudo -u hdfs hdfs dfs -copyToLocal /user/w205/tweets/$lasthr\.txt.gz /data/tmp/$lasthr\.txt.gz
 sudo chmod 777 /data/tmp/$lasthr\.txt.gz
-sudo -u hdfs hdfs dfs -cat /user/w205/tweets/$lasthr*.js | gzip >> /data/tmp/$lasthr\.txt.gz
+sudo -u hdfs hdfs dfs -cat /user/w205/raw/tweets/$lasthr*.js | gzip >> /data/tmp/$lasthr\.txt.gz
 
 fsize=$(wc -c <"$lasthr\.txt.gz")
 if [ $fsize -gt 20 ]
 then
- sudo -u hdfs hdfs dfs -rm /user/w205/tweets/$lasthr*.js
+ sudo -u hdfs hdfs dfs -rm /user/w205/raw/tweets/$lasthr*.js
  sudo -u hdfs hdfs dfs -moveFromLocal -f /data/tmp/$lasthr\.txt.gz /user/w205/tweets/$lasthr\.txt.gz
 fi
 
