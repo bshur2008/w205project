46b72445cf6b4a1d77b44de24f4a587954f8a9f0
Brandon Shurick <bshurick@berkeley.edu>
2015-12-05 17:27:49 +0000

    fix repair file

--- repair_last_hour.sh
+++ repair_last_hour.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 lasthr=$(date -d'-1 hour' +%Y%m%d%H)
 lastday=$(date -d'-1 day' +%Y%m%d)
+segments=(0 1 2 3 4 5)
 
-#sudo rm /data/tmp/$lasthr\.txt.gz
+sudo rm /data/tmp/$lasthr\.txt.gz
 sudo -u hdfs hdfs dfs -copyToLocal /user/w205/tweets/$lasthr\.txt.gz /data/tmp/$lasthr\.txt.gz
 sudo chmod 777 /data/tmp/$lasthr\.txt.gz
-sudo -u hdfs hdfs dfs -cat /data/tmp/raw/tweets/$lasthr*.js | gzip >> /data/tmp/$lasthr\.txt.gz
 
-fsize=$(wc -c <"$lasthr\.txt.gz")
+for s in ${segments[@]}
+do
+cat /data/tmp/raw/tweets/$lasthr${s:-1}*.js | gzip >> /data/tmp/$lasthr.txt.gz
+rm /data/tmp/raw/tweets/$lasthr${s:-1}*.js
+
+fsize=$(wc -c <"/data/tmp/$lasthr.txt.gz")
 if [[ $fsize -gt 20 ]]
 then
- #sudo -u hdfs hdfs dfs -rm /user/w205/raw/tweets/$lasthr*.js
- sudo -u hdfs hdfs dfs -moveFromLocal -f /data/tmp/$lasthr\.txt.gz /user/w205/tweets/$lasthr\.txt.gz
+ sudo -u hdfs hdfs dfs -moveFromLocal -f /data/tmp/$lasthr.txt.gz /user/w205/tweets/$lasthr.txt.gz
 fi
 
+done
