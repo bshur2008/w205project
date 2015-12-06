8c8d8190ddae4ed6fba7a50045323aad29c5025e
Brandon Shurick <bshurick@berkeley.edu>
2015-12-06 16:46:52 +0000

    updated ETL bash script

--- tweet_ETLs.sh
+++ tweet_ETLs.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+# DDL statements
+hive -f /project/DDLs/tweet_ddl.sql
+
+# ETL flow
 hive -f /project/ETLs/tweets_load_from_raw.sql 
 hive -f /project/ETLs/tweets_load_tweet_log.sql
 
