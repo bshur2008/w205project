329526fc194d3375b401fcd0607b9aeaa7b341c1
Brandon Shurick <bshurick@berkeley.edu>
2015-12-06 10:16:51 +0000

    try to fix cron

--- repair_last_hour.sh
+++ repair_last_hour.sh
@@ -3,9 +3,9 @@ lasthr=$(date -d'-1 hour' +%Y%m%d%H)
 lastday=$(date -d'-1 day' +%Y%m%d)
 segments=(0 1 2 3 4 5)
 
-sudo rm /data/tmp/$lasthr\.txt.gz
-sudo -u hdfs hdfs dfs -copyToLocal /user/w205/tweets/$lasthr\.txt.gz /data/tmp/$lasthr\.txt.gz
-sudo chmod 777 /data/tmp/$lasthr\.txt.gz
+rm /data/tmp/$lasthr.txt.gz
+hdfs dfs -copyToLocal /user/w205/tweets/$lasthr\.txt.gz /data/tmp/$lasthr\.txt.gz
+sudo chmod 777 /data/tmp/$lasthr.txt.gz
 
 for s in ${segments[@]}
 do
@@ -13,9 +13,8 @@ cat /data/tmp/raw/tweets/$lasthr$s*.js | gzip >> /data/tmp/$lasthr.txt.gz
 rm /data/tmp/raw/tweets/$lasthr$s*.js
 done
 
-fsize=$(wc -c <"/data/tmp/$lasthr.txt.gz")
-if [[ $fsize -gt 20 ]]
-then
- sudo -u hdfs hdfs dfs -moveFromLocal -f /data/tmp/$lasthr.txt.gz /user/w205/tweets/$lasthr.txt.gz
-fi
+#fsize=$(wc -c <"/data/tmp/$lasthr.txt.gz")
+#if [[ $fsize -gt 20 ]]; then
+hdfs dfs -moveFromLocal -f /data/tmp/$lasthr.txt.gz /user/w205/tweets/$lasthr.txt.gz
+#fi
 
