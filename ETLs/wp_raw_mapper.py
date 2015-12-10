b5f2efd8eee41b25e7d00a342a35ae85b3902944
Brandon Shurick <bshurick@berkeley.edu>
2015-12-10 22:13:38 +0000

    attempt fix for words

--- ETLs/wp_raw_mapper.py
+++ ETLs/wp_raw_mapper.py
@@ -7,18 +7,19 @@ for line in sys.stdin:
 		line = line.strip()
 		title, revisions = line.split('\t')
 		js = json.loads(revisions)
-		text = js['text']
+		text = js['text'].encode('utf-8')
 		title_words = title.split()
 		for title_word in title_words:
 			if re.search(ur'[^\w+]',title_word,flags=re.UNICODE): raise Exception('Bad title')
 		new_words = []
-		words = re.sub(ur'[\r\n\t]',' ',text).split()
+		words = re.sub(ur'[^\w+]',' ',text,flags=re.UNICODE).split()
 		for word in words:
-			if re.search(ur'[^\w+]',word,flags=re.UNICODE): continue
+			if word.startswith('http'): continue
 			if re.search(ur'[0-9\_]',word,flags=re.UNICODE): continue
 			new_words.append(word)
-		new_text = ' '.join(new_words) #.encode('utf-8')
+		new_text = ' '.join(new_words).decode('utf-8')
 		print('\t'.join([title,new_text]))
 	except Exception as e:
 		# print(e)
 		pass
+
