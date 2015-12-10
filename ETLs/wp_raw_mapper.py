2258b57e559d026a30d85f6e492924be930836ae
Brandon Shurick <bshurick@berkeley.edu>
2015-12-10 15:19:31 +0000

    assume unicode input for now

--- ETLs/wp_raw_mapper.py
+++ ETLs/wp_raw_mapper.py
@@ -9,13 +9,13 @@ for line in sys.stdin:
 		for title_word in title_words:
 			if re.search(ur'[^\w+]',title_word,flags=re.UNICODE): raise Exception('Bad title')
 		new_words = []
-		words = re.sub(ur'[\r\n\t]','',text.decode('utf-8')).split()
+		words = re.sub(ur'[\r\n\t]','',text).split()
 		for word in words:
 			if re.search(ur'[^\w+]',word,flags=re.UNICODE): continue
 			if re.search(ur'[0-9\_]',word,flags=re.UNICODE): continue
 			new_words.append(word)
-		new_text = ' '.join(new_words).encode('utf-8')
+		new_text = ' '.join(new_words) #.encode('utf-8')
 		print '\t'.join([title,new_text])
 	except Exception as e:
-		#print e
+		# print e
 		pass
