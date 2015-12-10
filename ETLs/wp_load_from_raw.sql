97846a98f1e51c01173815020dea3b585761b2a3
Brandon Shurick <bshurick@berkeley.edu>
2015-12-10 15:05:49 +0000

    attempt at solving unicode problems for wp data

--- ETLs/wp_load_from_raw.sql
+++ ETLs/wp_load_from_raw.sql
@@ -2,12 +2,11 @@ add jar /project/libs/hivexmlserde-1.0.5.3.jar;
 
 FROM (
 FROM raw.es_wikipedia
-SELECT TRANSFORM(es_wikipedia.title, es_wikipedia.revision)
+SELECT TRANSFORM(es_wikipedia.title, es_wikipedia.revision['text'])
 USING '/project/ETLs/wp_raw_mapper.py'
-AS (
+AS 
 title string
 , text string
-)
 ) raw_wp
 INSERT OVERWRITE TABLE prd.wikipedia
 SELECT 
--- ETLs/wp_raw_mapper.py
+++ ETLs/wp_raw_mapper.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 import sys, re
 
-for title, text in sys.stdin:
-	new_words = []
-	words = re.sub(r'[\r\n\t]','',text).split()
-	for word in words:
-		if "[" in word: continue
-		if "{" in word: continue
-		if word.startswith("http://"): continue
-		if "@" in word: continue
-		if "-" in word: continue
-		if "=" in word: continue
-		if "&" in word: continue
-		if "|" in word: continue
-		word = re.sub('[^\w+]','',word,flags=re.UNICODE)
-		new_words.append(word)
-	new_text = ' '.join(new_words)
-	print '\t'.join([title,new_text])
-
+for line in sys.stdin:
+	try:
+		line = line.strip()
+		title, text = line.split('\t')
+		title_words = title.split()
+		for title_word in title_words:
+			if re.search(ur'[^\w+]',title_word,flags=re.UNICODE): raise Exception('Bad title')
+		new_words = []
+		words = re.sub(ur'[\r\n\t]','',text.decode('utf-8')).split()
+		for word in words:
+			if re.search(ur'[^\w+]',word,flags=re.UNICODE): continue
+			if re.search(ur'[0-9\_]',word,flags=re.UNICODE): continue
+			new_words.append(word)
+		new_text = ' '.join(new_words).encode('utf-8')
+		print '\t'.join([title,new_text])
+	except Exception as e:
+		#print e
+		pass
