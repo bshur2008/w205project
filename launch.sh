155044f3c26d8c3fae60bd458a85ccf240cb423d
Brandon Shurick <bshurick@berkeley.edu>
2015-12-12 20:20:22 +0000

    try to make code agnostic for root directory

--- launch.sh
+++ launch.sh
@@ -2,6 +2,6 @@
 cnt="$(ps aux | grep lein | wc -l)"
 if [ $cnt -eq 1 ]
 then
-cd /project/lang_analysis
+cd ./lang_analysis
 lein run -m streamparse.commands.run/-main topologies/spanish_tweets.clj -t 0 --option 'topology.workers=2' --option 'topology.acker.executors=2' --option 'streamparse.log.path="/data/tmp/logs"' --option 'streamparse.log.level="error"'
 fi
--- tweet_ETLs.sh
+++ tweet_ETLs.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # DDL statements
-hive -f /project/DDLs/tweet_ddl.sql
+hive -f ./DDLs/tweet_ddl.sql
 
 # ETL flow
-hive -f /project/ETLs/tweets_load_from_raw.sql 
-hive -f /project/ETLs/tweets_load_tweet_log.sql
-hive -f /project/ETLs/tweets_load_tweet_users.sql
-hive -f /project/ETLs/tweets_load_tweet_words.sql
-hive -f /project/ETLs/combined_table.sql
+hive -f ./ETLs/tweets_load_from_raw.sql 
+hive -f ./ETLs/tweets_load_tweet_log.sql
+hive -f ./ETLs/tweets_load_tweet_users.sql
+hive -f ./ETLs/tweets_load_tweet_words.sql
+hive -f ./ETLs/combined_table.sql
 
 # POSTGRES
 sudo -u postgres psql -f DDLs/postgres_wc.sql
