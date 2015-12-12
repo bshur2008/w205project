2dbdfefc28efd4951618db8a34a3fe879480863e
Brandon Shurick <bshurick@berkeley.edu>
2015-12-12 19:50:27 +0000

    updated logic for moving files

--- move_last_hr_to_hdfs.sh
+++ move_last_hr_to_hdfs.sh
@@ -5,9 +5,8 @@ rm /data/tmp/$lasthr.txt.gz
 hdfs dfs -copyToLocal /user/w205/tweets/$lasthr\.txt.gz /data/tmp/$lasthr\.txt.gz
 sudo chmod 777 /data/tmp/$lasthr.txt.gz
 
-cat /data/tmp/raw/tweets/$lasthr.txt | gzip >> /data/tmp/$lasthr.txt.gz
+gzip /data/tmp/raw/tweets/$lasthr.txt 
+mv /data/tmp/raw/tweets/$lasthr.txt.gz /data/tmp/$lasthr.txt.gz
 
 hdfs dfs -moveFromLocal -f /data/tmp/$lasthr.txt.gz /user/w205/tweets/$lasthr.txt.gz
 
-#gzip /data/tmp/raw/tweets/$lasthr.txt
-rm /data/tmp/raw/tweets/$lasthr.txt
