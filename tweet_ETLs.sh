4b6b7115a91847d19cca9d9fa24d24d82276c52d
Brandon Shurick <bshurick@berkeley.edu>
2015-12-13 10:42:22 +0000

    remove postgres table

--- tweet_ETLs.sh
+++ tweet_ETLs.sh
@@ -11,5 +11,3 @@ hive -f $PROJECT_HOME/ETLs/tweets_load_tweet_users.sql
 hive -f $PROJECT_HOME/ETLs/tweets_load_tweet_words.sql
 hive -f $PROJECT_HOME/ETLs/combined_table.sql
 
-# POSTGRES
-sudo -u postgres psql -f $PROJECT_HOME/DDLs/postgres_wc.sql
