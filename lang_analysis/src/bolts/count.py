5aea59aa2409d1572c1360145e604025dc402159
Brandon Shurick <bshurick@berkeley.edu>
2015-12-13 08:54:24 +0000

    use redis instead of postgres

--- lang_analysis/src/bolts/count.py
+++ lang_analysis/src/bolts/count.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from collections import Counter
 from streamparse.bolt import Bolt
-import psycopg2
+import redis
 import datetime as DT
 TBL='tweet_word_count'
 
@@ -13,28 +13,9 @@ class TweetCounter(Bolt):
 		word = tup.values[0]
 		word = word.lower()
 		
-		conn = psycopg2.connect("user=postgres")
-		cur = conn.cursor()
-		cur.execute('''SELECT * from public.{}
-			WHERE word='{}' and language='{}';'''.format(
-				TBL, word, 'es'))
-		if cur.fetchone():
-			SQL = '''UPDATE public.{} 
-				SET cnt=cnt+{}
-				WHERE word='{}'
-				AND language='{}';
-			'''.format(TBL,1,word,
-				'es')
-		else:
-			SQL = '''INSERT INTO public.{}
-				(language, word, cnt)
-				VALUES ('{}','{}',{});
-				'''.format(TBL,'es',
-						word, 1)
-		cur.execute(SQL+'COMMIT;')
-		conn.commit()
-		cur.close()
-		conn.close()
+		# increment word in redis
+		r = redis.StrictRedis(host='localhost', port=6379, db=0)
+		r.incr(word)
 		
 		# emit
 		# Increment the local count
