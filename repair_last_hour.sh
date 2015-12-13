1f02da3e6ed1f7cffc35caedc4b174853542197c
Brandon Shurick <bshurick@berkeley.edu>
2015-12-13 09:40:24 +0000

    load to es dir

--- repair_last_hour.sh
+++ repair_last_hour.sh
@@ -4,7 +4,7 @@ lastday=$(date -d'-1 day' +%Y%m%d)
 segments=(0 1 2 3 4 5)
 
 rm /data/tmp/$lasthr.txt.gz
-hdfs dfs -copyToLocal /user/w205/tweets/$lasthr\.txt.gz /data/tmp/$lasthr\.txt.gz
+hdfs dfs -copyToLocal /user/w205/tweets/es/$lasthr\.txt.gz /data/tmp/$lasthr\.txt.gz
 sudo chmod 777 /data/tmp/$lasthr.txt.gz
 
 for s in ${segments[@]}
@@ -15,5 +15,5 @@ done
 
 #fsize=$(wc -c <"/data/tmp/$lasthr.txt.gz")
 #if [[ $fsize -gt 20 ]]; then
-hdfs dfs -moveFromLocal -f /data/tmp/$lasthr.txt.gz /user/w205/tweets/$lasthr.txt.gz
+hdfs dfs -moveFromLocal -f /data/tmp/$lasthr.txt.gz /user/w205/tweets/es/$lasthr.txt.gz
 #fi
