7e759a0e713b549be7090a80a8d030c70b68bbe1
Brandon Shurick <bshurick@berkeley.edu>
2015-12-15 17:57:47 +0000

    repair in smaller chunks

--- repair_last_hour.sh
+++ repair_last_hour.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 lasthr=$(date -d'-1 hour' +%Y%m%d%H)
+lasthr=2015121511
 segments=(0 1 2 3 4 5)
+segments2=(0 1 2 3 4 5 6 7 8 9)
 
 rm /data/tmp/$lasthr.txt.gz
 hdfs dfs -copyToLocal /user/w205/tweets/es/$lasthr\.txt.gz /data/tmp/$lasthr\.txt.gz
@@ -8,11 +10,14 @@ sudo chmod 777 /data/tmp/$lasthr.txt.gz
 
 for s in ${segments[@]}
 do
-cat /data/tmp/raw/tweets/$lasthr$s*.js | gzip >> /data/tmp/$lasthr.txt.gz
-rm /data/tmp/raw/tweets/$lasthr$s*.js
+
+for ss in ${segments2[@]}
+do
+
+cat /data/tmp/raw/tweets/$lasthr$s$ss*.js | gzip >> /data/tmp/$lasthr.txt.gz
+rm /data/tmp/raw/tweets/$lasthr$s$ss*.js
+
+done
 done
 
-#fsize=$(wc -c <"/data/tmp/$lasthr.txt.gz")
-#if [[ $fsize -gt 20 ]]; then
 hdfs dfs -moveFromLocal -f /data/tmp/$lasthr.txt.gz /user/w205/tweets/es/$lasthr.txt.gz
-#fi
