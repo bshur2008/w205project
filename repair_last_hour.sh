d868d2f3ac081b7497ccb64b92f521c863c77aed
Brandon Shurick <bshurick@berkeley.edu>
2015-12-19 07:35:59 +0000

    add streaming job to repair last hour job

--- repair_last_hour.sh
+++ repair_last_hour.sh
@@ -3,6 +3,7 @@ lasthr=$(date -d'-1 hour' +%Y%m%d%H)
 segments=(0 1 2 3 4 5)
 segments2=(0 1 2 3 4 5 6 7 8 9)
 
+# Load files into HDFS
 rm /data/tmp/$lasthr.txt.gz
 hdfs dfs -copyToLocal /user/w205/tweets/es/$lasthr\.txt.gz /data/tmp/$lasthr\.txt.gz
 sudo chmod 777 /data/tmp/$lasthr.txt.gz
@@ -20,3 +21,16 @@ done
 done
 
 hdfs dfs -moveFromLocal -f /data/tmp/$lasthr.txt.gz /user/w205/tweets/es/$lasthr.txt.gz
+
+
+# Run ETL
+: << 'END'
+sudo -u hdfs hdfs dfs -rm -r -f /user/w205/tweets/mapped
+sudo -u hdfs hadoop \
+jar /usr/lib/hadoop-mapreduce/hadoop-streaming-2.6.0-cdh5.4.7.jar \
+-input /user/w205/tweets/es/$lasthr.txt.gz \
+-output /user/w205/tweets/mapped \
+-mapper ETLs/tweets_raw_mapper.py \
+-file ETLs/tweets_raw_mapper.py
+hive -e "LOAD DATA INPATH '/user/w205/tweets/mapped' INTO TABLE stg.tweets PARTITION (hr=$lasthr);"
+END
--- tweet_ETLs.sh
+++ tweet_ETLs.sh
@@ -4,18 +4,8 @@ PROJECT_HOME=/project
 # DDL statements
 hive -f $PROJECT_HOME/DDLs/tweet_ddl.sql
 
-# Streaming job
-: << 'END'
-sudo -u hdfs hadoop \
-jar /usr/lib/hadoop-mapreduce/hadoop-streaming-2.6.0-cdh5.4.7.jar \
--input /user/w205/tweets/es/2015121511.txt.gz \
--output /user/w205/test \
--mapper ETLs/tweets_raw_mapper.py \
--file ETLs/tweets_raw_mapper.py
-END
-
 # ETL flow
-hive -f $PROJECT_HOME/ETLs/tweets_load_from_raw.sql 
+# hive -f $PROJECT_HOME/ETLs/tweets_load_from_raw.sql 
 hive -f $PROJECT_HOME/ETLs/tweets_load_tweet_log.sql
 hive -f $PROJECT_HOME/ETLs/tweets_load_tweet_users.sql
 hive -f $PROJECT_HOME/ETLs/tweets_load_tweet_words.sql
